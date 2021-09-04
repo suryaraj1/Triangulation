91433af67efe53ecd0fe505eb81d829b9801b395
Suryaraj Bhaduri <suryaraj_bhaduri@yahoo.com>
2021-09-04 23:29:22 +0530

    Add onChange handler and change background

--- src/components/InputComponent/InputComponent.js
+++ src/components/InputComponent/InputComponent.js
@@ -1,17 +1,37 @@
 import React from 'react';
 import './InputComponent.css';
 
-const InputComponent = ({ inputLabel }) => {
-    return (
-        <div className='input-wrapper'>
-            <div className='input-label-wrapper'>
-                <p className='input-label'>{inputLabel}</p>
+class InputComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            inputValue: ""
+        }
+    }
+
+    onChangeHandler = event => {
+        this.setState({
+            inputValue: event.target.value
+        }, () => {
+            const { handler } = this.props;
+            const { inputValue } = this.state;
+            handler(inputValue);
+        })
+    }
+
+    render() {
+        const { inputLabel } = this.props;
+        return (
+            <div className='input-wrapper'>
+                <div className='input-label-wrapper'>
+                    <p className='input-label'>{inputLabel}</p>
+                </div>
+                <div className='input-box-wrapper'>
+                    <input onChange={this.onChangeHandler}/>
+                </div>
             </div>
-            <div className='input-box-wrapper'>
-                <input />
-            </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default InputComponent;
