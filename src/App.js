0938b1cf18f65d944faa10182afa1c626d8a37f4
Suryaraj Bhaduri <suryaraj_bhaduri@yahoo.com>
2021-09-04 20:46:38 +0530

    Add LeftSection, RightSection to App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header/Header';
+import LeftSection from './components/LeftSection/LeftSection';
+import RightSection from './components/RightSection/RightSection';
 
 const App = () => {
     return (
         <div>
-            <Header />        
+            <LeftSection />  
+            <RightSection />      
         </div>
     )
 }
