96e35155e21b018acce01b0e6608b34e46a991b2
Suryaraj Bhaduri <suryaraj_bhaduri@yahoo.com>
2021-09-04 20:06:25 +0530

    Add Header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div className='header-wrapper'>
+            <p className='header-main'>Triangulation</p>
+            <p className='header-sub'>A web app dedicated to triangles.</p>
+        </div>
+    )
+}
+
+export default Header;
