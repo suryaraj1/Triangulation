60706726426003c2b82bde3a85489e20493a0e10
Suryaraj Bhaduri <suryaraj_bhaduri@yahoo.com>
2021-09-05 11:01:15 +0530

    Add Area calculator route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+import AreaCalculator from './pages/AreaCalculator/AreaCalculator';
 import Home from './pages/Home/Home';
 import HypotCalculator from './pages/HypotCalculator/HypotCalculator';
 
@@ -10,6 +11,7 @@ const App = () => {
             <Switch>
                 <Route exact path="/" component={Home} />
                 <Route exact path="/hypotenuse-calculator" component={HypotCalculator} />
+                <Route exact path="/area-calculator" component={AreaCalculator} />
             </Switch>
         </Router>
     )
