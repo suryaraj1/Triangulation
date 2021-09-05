c8d5d1e0a6c6cb7e068a6e81f469bc1b8d115fb4
Suryaraj Bhaduri <suryaraj_bhaduri@yahoo.com>
2021-09-05 12:45:31 +0530

    Add IsTriangle component

--- src/pages/IsTriangle/IsTriangle.js
+++ src/pages/IsTriangle/IsTriangle.js
@@ -4,10 +4,58 @@ import Footer from '../../components/Footer/Footer';
 import Header from '../../components/Header/Header';
 import Hint from '../../components/Hint/Hint';
 import InputComponent from '../../components/InputComponent/InputComponent';
+import ValidityAlert from '../../components/ValidityAlert/ValidityAlert';
 import './IsTriangle.css';
 
 class IsTriangle extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            firstAngle: "",
+            secondAngle: "",
+            thirdAngle: "",
+            isValidTriangle: false,
+            clickCount: 0
+        }
+    }
+
+    firstAngleHandler = angle => {
+        this.setState({
+            firstAngle: angle
+        });
+    }
+
+    secondAngleHandler = angle => {
+        this.setState({
+            secondAngle: angle
+        });
+    }
+
+    thirdAngleHandler = angle => {
+        this.setState({
+            thirdAngle: angle
+        });
+    }
+
+    checkTriangleValidity = (firstAngle, secondAngle, thirdAngle) => {
+        // enter input valdation logic here
+        let angleSummation = parseFloat(firstAngle);
+        angleSummation += parseFloat(secondAngle);
+        angleSummation += parseFloat(thirdAngle);
+        console.log(angleSummation);
+        return (angleSummation === 180.00);
+    }
+
+    onClickHandler = () => {
+        const { firstAngle, secondAngle, thirdAngle, clickCount } = this.state;
+        this.setState({
+            isValidTriangle: this.checkTriangleValidity(firstAngle, secondAngle, thirdAngle),
+            clickCount: clickCount + 1
+        })
+    }
+
     render() {
+        const { clickCount, isValidTriangle } = this.state;
         return (
             <div className="is-triangle-wrapper">
                 <div className='is-triangle-left-section'>
@@ -20,14 +68,17 @@ class IsTriangle extends React.Component {
                 <div className="is-triangle-right-section">
                     <div className='is-triangle-input-wrapper'>
                         <div className='is-triangle-input-container'>
-                            <InputComponent inputLabel="Enter first angle"/>
-                            <InputComponent inputLabel="Enter second angle" />
-                            <InputComponent inputLabel="Enter third angle" />
+                            <InputComponent inputLabel="Enter first angle" handler={this.firstAngleHandler}/>
+                            <InputComponent inputLabel="Enter second angle" handler={this.secondAngleHandler}/>
+                            <InputComponent inputLabel="Enter third angle" handler={this.thirdAngleHandler}/>
                         </div>
                         <div className='is-triangle-btn-wrapper'>
-                            <Button btnTitle="Check" />
+                            <Button btnTitle="Check" clickHandler={this.onClickHandler}/>
                         </div>
                     </div>
+                    <div className='is-triangle-validity-alert-wrapper'>
+                            {clickCount > 0 && <ValidityAlert success={isValidTriangle}/>}
+                        </div>
                 </div>
             </div>
         )
