040b8d9514dbd82c57718a6c1eafd94388703ea1
Suryaraj Bhaduri <suryaraj_bhaduri@yahoo.com>
2021-09-05 11:38:17 +0530

    Add route for IsTriangle app

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import AreaCalculator from './pages/AreaCalculator/AreaCalculator';
 import Home from './pages/Home/Home';
 import HypotCalculator from './pages/HypotCalculator/HypotCalculator';
+import IsTriangle from './pages/IsTriangle/IsTriangle';
 
 const App = () => {
     return (
@@ -12,6 +13,7 @@ const App = () => {
                 <Route exact path="/" component={Home} />
                 <Route exact path="/hypotenuse-calculator" component={HypotCalculator} />
                 <Route exact path="/area-calculator" component={AreaCalculator} />
+                <Route exact path="/is-triangle" component={IsTriangle} />
             </Switch>
         </Router>
     )
