da1f120ec61975b948aef6ed50fdcc34bfbb79d9
Suryaraj Bhaduri <suryaraj_bhaduri@yahoo.com>
2021-09-05 13:06:58 +0530

    Change text of failure message

--- src/components/ValidityAlert/ValidityAlert.js
+++ src/components/ValidityAlert/ValidityAlert.js
@@ -16,7 +16,7 @@ const ValidityAlert = ({ success }) => {
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
                     </svg>
-                    <p className='validity-alert-text failure'>Sorry! You cannot form a Triangle.😭</p>
+                    <p className='validity-alert-text failure'>Sorry! You cannot form a Triangle 😭</p>
                 </div>
             )}
         </div>
